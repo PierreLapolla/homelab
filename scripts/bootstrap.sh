7e8932ddfd39a34dcbd159d09390fc24d10268bf
pierre <plapolla9@gmail.com>
2024-10-21 22:09:33 +0200

    fix setup scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Run the git_check.sh script
+echo "Running git check script..."
+bash scripts/git_check.sh
+
 # Run the setup script to install Ansible (if not already done)
 echo "Running setup script..."
 bash scripts/setup.sh
