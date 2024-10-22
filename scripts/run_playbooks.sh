48bfbfdeb2189334f1afaef529fc17f34654dc1e
pierre <plapolla9@gmail.com>
2024-10-22 14:47:31 +0200

    fix playbook script

--- scripts/run_playbooks.sh
+++ scripts/run_playbooks.sh
@@ -6,6 +6,23 @@ SCRIPT_DIR=$(dirname "$0")
 # Navigate to the root directory of your project
 PROJECT_ROOT=$(cd "$SCRIPT_DIR/.." && pwd)
 
+# Deleting custom namespaces for a complete redeployment from scratch
+echo "Deleting custom namespaces to clean up all old resources..."
+
+# Delete monitoring namespace (Prometheus and Grafana)
+kubectl delete namespace monitoring --ignore-not-found
+
+# Delete default namespace
+kubectl delete namespace default --ignore-not-found
+
+# Wait for the namespaces to be fully deleted before proceeding
+for namespace in default monitoring; do
+    while kubectl get namespace $namespace 2>/dev/null; do
+        echo "Waiting for the $namespace namespace to be fully deleted..."
+        sleep 5
+    done
+done
+
 # Run dependencies.yml to install basic dependencies
 echo "Running dependencies playbook..."
 ansible-playbook -i "$PROJECT_ROOT/ansible/inventory/hosts.ini" "$PROJECT_ROOT/ansible/playbooks/dependencies.yml" || { echo "Failed to run dependencies playbook"; exit 1; }
