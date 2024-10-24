d8914c32f9d687200a2024a1cea85924df670925
pierre <plapolla9@gmail.com>
2024-10-24 13:19:54 +0200

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit the script on any error
+set -e
+
 # Update system packages
 echo "Updating system packages..."
 sudo apt update && sudo apt upgrade -y
@@ -9,15 +12,16 @@ echo "Installing K3s..."
 curl -sfL https://get.k3s.io | sh -
 
 # Wait for K3s to be ready
+echo "Waiting for K3s to be ready..."
 sleep 60
 
 # Install Flux CLI
 echo "Installing Flux CLI..."
 curl -s https://fluxcd.io/install.sh | sudo bash
 
-# Export KUBECONFIG
+# Export KUBECONFIG with elevated permissions
 echo "Exporting KUBECONFIG..."
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+sudo export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # Install Helm
 echo "Installing Helm..."
